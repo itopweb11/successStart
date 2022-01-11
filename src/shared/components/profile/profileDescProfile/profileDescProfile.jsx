d0d88de1f603783b650a3d1afa60030292614a2e
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-01-11 22:42:53 +0300

    User data

--- src/shared/components/profile/profileDescProfile/profileDescProfile.jsx
+++ src/shared/components/profile/profileDescProfile/profileDescProfile.jsx
@@ -6,6 +6,10 @@ import FileInn from "../../../img/svg/fileInn";
 import Place from "../../../img/svg/place";
 import RegistrationAddress from "../../../img/svg/registrationAddress";
 import Home from "../../../img/svg/home";
+import SeriesNumber from "../../../img/svg/SeriesNumber";
+import Scanner from "../../../img/svg/scanner";
+import IssuedBy from "../../../img/svg/IssuedBy";
+import EyeIcon from "../../../img/svg/EyeIcon";
 
 const ProfileDescProfile = () => {
     return(
@@ -50,6 +54,31 @@ const ProfileDescProfile = () => {
             </div>
             <div className='profile__passportData'>
                 <p className='profileData__title'>Паспортные данные</p>
+                <div className='profile__personalData__desc'>
+                    <SeriesNumber />
+                    <span>Серия и номер паспорта</span>
+                    <span>1111-111111</span>
+                </div>
+                <div className='profile__personalData__desc'>
+                    <DateBirth />
+                    <span>Дата выдачи</span>
+                    <span>1111-111111</span>
+                </div>
+                <div className='profile__personalData__desc'>
+                    <IssuedBy />
+                    <span>Кем выдан</span>
+                    <span>1111-111111</span>
+                </div>
+                <div className='profile__personalData__desc'>
+                    <EyeIcon />
+                    <span>Код подразделения</span>
+                    <span>1111-111111</span>
+                </div>
+                <div className='profile__personalData__desc'>
+                    <Scanner />
+                    <span>Сканы паспорта</span>
+                    <span>1111-111111</span>
+                </div>
             </div>
         </div>
     )
