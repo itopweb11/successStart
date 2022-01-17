1dcad1d3b28bda7167946e26a6a6ea20582cc599
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-01-17 19:53:01 +0300

    оптимизация кода

--- src/shared/components/investments/investments.jsx
+++ src/shared/components/investments/investments.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 const Investments = () => {
     return(
-        <div>
-            <div>
-                <img src="" alt="icon"/>
-                <p>Для того, чтобы получить доступ к инвестированию, необходимо аккредитовать профиль</p>
-                <button>Перейти к профилю</button>
+        <div className='Investments'>
+            <div className='Investments__header'>
+                <div>
+                    <img src="" alt="icon"/>
+                    <p>Для того, чтобы получить доступ к инвестированию, необходимо аккредитовать профиль</p>
+                    <button>Перейти к профилю</button>
+                </div>
+                <div>
+                    <p>90 000 000,00 ₽ Доступно</p>
+                    <p>0,00 ₽ В резерве</p>
+                    <button>Пополнить</button>
+                    <button>Вывести</button>
+                </div>
             </div>
-            <div>
-                <p>90 000 000,00 ₽ Доступно</p>
-                <p>0,00 ₽ В резерве</p>
-                <button>Пополнить</button>
-                <button>Вывести</button>
-            </div>
-            <div>
+            <div className='Investments__content'>
                 <p>Инвестиционные предложения</p>
                 <button>Сбор инвестиций</button>
                 <button>Сбор инвестиций завершен</button>
