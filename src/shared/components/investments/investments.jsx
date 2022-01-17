68508c50a83bbf467e44c0f114d39400ef6acba8
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-01-17 20:39:47 +0300

    Investments

--- src/shared/components/investments/investments.jsx
+++ src/shared/components/investments/investments.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import Accreditation from "../../img/svg/accreditation";
 
 const Investments = () => {
     return(
-        <div className='Investments'>
-            <div className='Investments__header'>
-                <div>
-                    <img src="" alt="icon"/>
+        <div className='investments'>
+            <div className='investments__header'>
+                <div className='investments__header__accreditation'>
+                    <Accreditation />
                     <p>Для того, чтобы получить доступ к инвестированию, необходимо аккредитовать профиль</p>
                     <button>Перейти к профилю</button>
                 </div>
-                <div>
-                    <p>90 000 000,00 ₽ Доступно</p>
-                    <p>0,00 ₽ В резерве</p>
-                    <button>Пополнить</button>
-                    <button>Вывести</button>
+                <div className='investments__header__check'>
+                    <div className='investments__header__check__desc'>
+                        <p>90 000 000,00 ₽ <span>Доступно</span></p>
+                        <p>0,00 ₽ <span>В резерве</span></p>
+                    </div>
+                    <div className='investments__header__check__buttons'>
+                        <button>Пополнить</button>
+                        <button>Вывести</button>
+                    </div>
                 </div>
             </div>
-            <div className='Investments__content'>
+            <div className='investments__content'>
                 <p>Инвестиционные предложения</p>
                 <button>Сбор инвестиций</button>
                 <button>Сбор инвестиций завершен</button>
