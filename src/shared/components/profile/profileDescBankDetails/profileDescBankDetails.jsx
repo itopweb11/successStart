c10f0917153c2901b6b0981f282dfad067574b8e
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-01-21 20:49:05 +0300

    Bank details data

--- src/shared/components/profile/profileDescBankDetails/profileDescBankDetails.jsx
+++ src/shared/components/profile/profileDescBankDetails/profileDescBankDetails.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import IssuedBy from "../../../img/svg/IssuedBy";
-import SeriesNumber from "../../../img/svg/SeriesNumber";
-import DateBirth from "../../../img/svg/dateBirth";
-import EyeIcon from "../../../img/svg/EyeIcon";
-import Scanner from "../../../img/svg/scanner";
+import DataForm from "../../DataForm";
 
 const ProfileDescBankDetails = ({data}) => {
     const bankDetails = data?.bank_details[0];
     console.log(bankDetails)
 
     const dataFormat = [
-        {id: 0, key: 'name', label: 'Наименование банка или БИК', value: bankDetails.name, Icon: IssuedBy},
-        {id: 1, key: 'bik', label: 'БИК', value: bankDetails.bik, Icon: IssuedBy},
-        {id: 2, key: 'inn', label: 'ИНН банка', value: bankDetails.inn, Icon: IssuedBy},
-        {id: 3, key: 'correspondent_account', label: 'Корр. счет', value: bankDetails.correspondent_account, Icon: IssuedBy},
-        {id: 4, key: 'checking_account', label: 'Расчетный счет', value: bankDetails.checking_account, Icon: IssuedBy}
+        {id: 0, key: 'name', label: 'Наименование банка или БИК', value: bankDetails.name ? bankDetails.name : 'Заполните поле' , Icon: IssuedBy},
+        {id: 1, key: 'bik', label: 'БИК', value: bankDetails.bik ? bankDetails.bik : 'Заполните поле', Icon: IssuedBy},
+        {id: 2, key: 'inn', label: 'ИНН банка', value: bankDetails.inn ? bankDetails.inn : 'Заполните поле', Icon: IssuedBy},
+        {id: 3, key: 'correspondent_account', label: 'Корр. счет', value: bankDetails.correspondent_account ? bankDetails.correspondent_account : 'Заполните поле', Icon: IssuedBy},
+        {id: 4, key: 'checking_account', label: 'Расчетный счет', value: bankDetails.checking_account ? bankDetails.checking_account : 'Заполните поле', Icon: IssuedBy}
     ]
 
     return(
         <div className='profileProps'>
+            <DataForm dataForm={dataFormat} formTitle="Счет" />
+            <div className='profileProps__info'>
+                <img src="https://www.svgrepo.com/show/187899/credit-card-payment.svg" alt="credit card"/>
+                <p className='profileProps__info__title profileProps__title'>Реквизиты банковского счета необходимы для вывода денежных средств с вашего счета</p>
+            </div>
+        </div>
+
+    /*    <div className='profileProps'>
             <div className='profileProps__bankDetails'>
                 <p className='profileProps__title'>Счет</p>
                 <div className='profileProps__bankDetails__item'>
@@ -47,11 +52,8 @@ const ProfileDescBankDetails = ({data}) => {
                     <span>Заполните поле</span>
                 </div>
             </div>
-            <div className='profileProps__info'>
-                <img src="https://www.svgrepo.com/show/187899/credit-card-payment.svg" alt="credit card"/>
-                <p className='profileProps__info__title profileProps__title'>Реквизиты банковского счета необходимы для вывода денежных средств с вашего счета</p>
-            </div>
-        </div>
+
+        </div>*/
     )
 }
 
