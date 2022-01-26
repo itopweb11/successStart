2a4c36acd1de39ae14dec2e1d761346b34595a64
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-01-26 20:03:01 +0300

    оптимизация кода

--- src/shared/components/profile/profile.jsx
+++ src/shared/components/profile/profile.jsx
@@ -57,7 +57,6 @@ const Profile = ({data, setData, menu}) => {
                             alt="vector"
                         />
                     </div>
-                    <span className='profile__user__line'></span>
                     <div className={navbarButton}>
                         <button>
                             <div className='profile__user__buttonPros'>
