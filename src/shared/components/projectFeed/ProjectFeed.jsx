794198690a0a961371d1eadda930a7b182731016
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-02-17 00:47:37 +0300

    ProjectFeed

--- src/shared/components/projectFeed/ProjectFeed.jsx
+++ src/shared/components/projectFeed/ProjectFeed.jsx
@@ -16,6 +16,7 @@ const ProjectFeed = () => {
                 </button>
             </div>
             <div>
+                <p>Лента проектов</p>
                 <div>
                     <span>Сбор инвестиций</span>
                     <span>Сбор инвестиций завершен</span>
