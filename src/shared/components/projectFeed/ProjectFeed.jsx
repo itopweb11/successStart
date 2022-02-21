763f5e3f85c7e8cf4f18a1172aa30d4aad7251ff
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-02-21 22:35:41 +0300

    ProjectFeed

--- src/shared/components/projectFeed/ProjectFeed.jsx
+++ src/shared/components/projectFeed/ProjectFeed.jsx
@@ -1,14 +1,14 @@
 import ProjectList from "./projectList/projectList";
 import ProjectCompleted from "./projectCompleted/projectCompleted";
-import {Label} from "reactstrap";
+import PlusSquare from "../../img/svg/plusSquare";
 
 const ProjectFeed = () => {
 
     return (
         <div className='projectFeed'>
             <div className='projectFeed__addProject'>
-                <div>
-                    <div></div>
+                <div className='projectFeed__addProject__desc'>
+                    <PlusSquare />
                     <p>Создайте свой инвестиционный проект и привлеките средства для вашего бизнеса.</p>
                 </div>
                 <button>
---
+++ src/shared/img/svg/plusSquare.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PlusSquare = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-plus-square">
+            <g>
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
+                <line x1="12" y1="8" x2="12" y2="16"/>
+                <line x1="8" y1="12" x2="16" y2="12"/>
+            </g>
+        </svg>
+    )
+}
+
+export default PlusSquare;
